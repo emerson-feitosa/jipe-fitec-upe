b6444ff30adb47d4c90c3d327f98bf9489e4f43d
rikcav <henriquearau000@gmail.com>
2024-02-22 15:30:47 -0300

    refactor: anotação EntityScan

--- src/main/java/br/upe/ecoenergy/EcoenergyApplication.java
+++ src/main/java/br/upe/ecoenergy/EcoenergyApplication.java
@@ -2,12 +2,14 @@ package br.upe.ecoenergy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("br.upe.ecoenergy.domain")
 public class EcoenergyApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcoenergyApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcoenergyApplication.class, args);
+    }
 
 }
